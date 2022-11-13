569f8885b19e0f74696897faaa44d3d4cd631ad2
Dmitry Rodichev <ya.vilnes@yandex.ru>
2022-11-14 02:59:46 +0600

    Delete extra ots

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -1,9 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-
 import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
